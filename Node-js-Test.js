d2c4bd144e11c1fe2c06de40e00b4ec5660e4bf3
Jose Arias <115059715+Monoarias777@users.noreply.github.com>
2023-05-06 13:56:39 -0400

    JS Basis with NODE js

---
+++ Node-js-Test.js
@@ -0,0 +1,53 @@
+console.log("Hello, mates");
+console.log(10);
+console.log(2 * 5);
+console.log("Jose " + "Arias");
+console.log(5 + 2 * 10);
+
+const firstName = "Jose";
+const lastName = "Arias";
+console.log("Hello, " + firstName + " " + lastName + ".");
+
+let a = 8;
+let b = 3;
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+
+
+console.log("sum:", sum);
+console.log("difference:", difference);
+console.log("product:", product);
+console.log("quotient:", quotient);
+
+a = 15;
+b = 45;
+
+console.log("sum:", sum);
+console.log("difference:", difference);
+console.log("product:", product);
+console.log("quotient:", quotient);
+
+const finalString = firstName + " " + lastName;
+
+console.log('finalString: ', finalString);
+
+
+let x = 0;
+let y = 1;
+
+console.log('x === y', x === y);
+x = 10;
+y = 15;
+console.log('x > y:', x > y);
+x = 12;
+y = 2;
+console.log('x === -1 && y === x:', x === -1 && y === x);
+x = 55;
+y = 24;
+console.log('x > 0 && y === 24:', x > 0 && y === 24);
+y = 24;
+console.log('!(y === 24):', !(y === "24"));
+x = 12;
+console.log('x * 2 === y:', x * 2 === y);
